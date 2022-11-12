cdd9131d1f35d0971c9661126aa141500eb45319
denis <pdv2008@gmail.com>
2022-11-12 09:53:25 +0200

    Lesson

---
+++ 29 (js)/assets/js/norma.js
@@ -0,0 +1,16 @@
+// 1. Користувач вводить суму грошей в гаманці і ціну однієї шоколадки. Програма виводить скільки шоколадок може купити користувач і скільки здачі у нього залишиться.
+
+let moneyInWallet = prompt("Сколько денег у вас в кошельке?", '' );
+let chocolatePrice = prompt("Сколько стоит одна шоколадка?", '' );
+
+let amountOfChocolate = Math.floor(moneyInWallet / chocolatePrice);
+let moneyIsLeft = moneyInWallet - (chocolatePrice * amountOfChocolate);
+
+alert(`Вы можете купить ${amountOfChocolate} шоколадки`);
+alert(`Денег у вас останется ${moneyIsLeft}`);
+
+
+// 2. Запитай у користувача тризначне число і виведи його задом наперед.
+
+let number = prompt ("Введите трехзначное число, ответом будет его реверс", '');
+alert(number.split("").reverse().join(""));
