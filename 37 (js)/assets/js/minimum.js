61eeba33147369e3e2815c77db2c26e8b14e500a
denis <pdv2008@gmail.com>
2022-11-26 03:55:26 +0200

    Lessons

---
+++ 37 (js)/assets/js/minimum.js
@@ -0,0 +1,39 @@
+/* function checkTrueOrFalse(value, yes, no) {
+    if(value) {
+      yes()
+    } else {
+      no()
+    }
+}
+
+
+function showTrue() {
+  console.log('True state')
+}
+
+function showFAlse () {
+  console.log('False state')
+}
+
+checkTrueOrFalse(confirm('Press ok or cancel') showTrue, showFAlse ) */
+
+
+loadPage();
+
+function loadPage() {
+    let xhr = new XMLHttpRequest;
+
+    xhr.open('GET', 'pages/main.html')
+    xhr.send()
+
+
+    /* console.log(xhr)
+    console.log(xhr.response) */
+
+    xhr.onreadystatechange = function() {
+        console.log(xhr)
+        if(xhr.readyState === 4) {
+          console.log(xhr.response)
+        }
+    }
+}
