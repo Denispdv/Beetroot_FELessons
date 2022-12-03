01d94838f7501093e21c654e20ee28a459e4fc75
denis <pdv2008@gmail.com>
2022-12-03 19:54:34 +0200

    Lesson

--- project_2/assets/js/index.js
+++ project_2/assets/js/index.js
@@ -2,15 +2,13 @@ $('.slider').slick({
   infinite:true
 })
 
-
-
 $(document).ready(function () {
   $('.single-item').slick({
       dots: true,
       vertical: true,
       verticalSwiping: true,
       adaptiveHeight: true,
-      arrows: false,
+      arrows: true,
       autoplay: true,
       autoplaySpeed: 7000,
   });
@@ -24,3 +22,28 @@ $(document).ready(function () {
       autoplaySpeed: 7000,
   });
 });
+
+/* const swiper = new Swiper('.slider', {
+  // Optional parameters
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+}); */
+
+/* $(document).ready(function(){
+  $('.slider').slick({
+    arrows:true,
+    dots:true,
+  
+  });
+}); */
